167e1188792f640c1a9575f462c45a2d6e9137d5
MathBern <matheus.b.peron@gmail.com>
2020-10-02 11:09:30 -0300

    SWORD: L; HORSE: 17.3, 25 and 25.5 kK; THRONE: 21.5kK and 23kK

---
+++ THRONE/clean_all_dirs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+for d in */; do
+    cd $d
+    #
+    # Shell procedure to clean up model directory
+    # of unecessry files after a model has been
+    # completed.
+    #
+    rm BAION
+    rm BAMAT
+    rm BAIONPNT
+    rm BAMATPNT
+    rm BA_STEQ
+    rm BA_ASCI_N_D*
+    #
+    rm EDDFACTOR
+    rm EDDFACTOR_INFO
+    rm J_COMP
+    rm JEW
+    #
+    rm EWDATA
+    rm STEQ_VALS
+    rm LINEHEAT
+    rm NETRATE
+    rm TOTRATE
+    rm TRANS_INFO
+    #
+    rm CFDAT_OUT
+    rm CONT_FREQ
+    rm *SCRATCH*
+    #
+    rm fort.63
+    rm fort.88
+    rm fort.53
+
+    rm -f *SCRA* BA* POINT* SCRTEMP fort* *RRR* NETRATE LINEHEAT TOTRATE *RRR*
+
+    find * -type l -maxdepth 0 -exec rm -vf {} ';' #rmlinks
+   
+    cd obs
+    rm -f *SCRA* BA* POINT* SCRTEMP fort* *RRR* NETRATE LINEHEAT TOTRATE *RRR*
+    find * -type l -maxdepth 0 -exec rm -vf {} ';' #rmlinks
+    rm MOM_J_ERRORS
+    rm core
+    cd ..
+    
+    cd lte
+    rm -f *SCRA* BA* POINT* SCRTEMP fort* *RRR* NETRATE LINEHEAT TOTRATE *RRR*
+    find * -type l -maxdepth 0 -exec rm -vf {} ';' #rmlinks
+    rm MOM_J_ERRORS
+    rm POPIRON
+    cd ..
+    
+    cd ..
+done
+
